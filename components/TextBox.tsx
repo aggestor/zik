525f9a7614bb61cc715ef2b1ce25e5ae3ae819f6
Aggestor <aggeer.aa@gmail.com>
2023-05-10 23:04:51 +0300

    add: change event on textbox

--- components/TextBox.tsx
+++ components/TextBox.tsx
@@ -4,11 +4,12 @@ declare type Props = {
     placeholder?: string,
     name:string
     type?:string
+    onChange?:(e:any)=> void
 }
-const TextBox: FunctionComponent<Props> = ({label, placeholder, type = "text", name}) =>{
+const TextBox: FunctionComponent<Props> = ({label, placeholder, type = "text", name,onChange}) =>{
     return <div className=" focus-within:text-blue-600 transition-all duration-[400] focus-within:font-semibold my-2 text-gray-600 flex flex-col">
             <label htmlFor={name} className="ml-1">{label}</label>
-            <input className="h-9 transition-all duration-300 focus-within:ring-2 focus:ring-blue-600 px-1.5 border rounded-lg outline-none text-gray-700" name={name} id={name} placeholder={placeholder} type={type}/>
+            <input onChange={onChange} className="h-9 transition-all duration-300 focus-within:ring-2 focus:ring-blue-600 px-1.5 border rounded-lg outline-none text-gray-700" name={name} id={name} placeholder={placeholder} type={type}/>
     </div>
 }
 
