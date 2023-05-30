f05eada26083eb4d1933dfd2536c33e7f72402f1
aggestor <aggeemhl@gmail.com>
2023-05-30 20:15:18 +0200

    add: children into buttons

--- app/su/categories/page.tsx
+++ app/su/categories/page.tsx
@@ -1,9 +1,13 @@
 "use client"
 import GoBack from "@/components/GoBack";
 import { useState } from "react";
-import { BsFilter, BsGrid3X3, BsList, BsListOl } from "react-icons/bs";
+import { BsFilter, BsGrid3X3, BsList, BsListOl, BsPlus } from "react-icons/bs";
 import UserCard from "../components/UserCard";
+import { BaseBlueButton, SmallBlueButton } from "@/components/BlueButtons";
 
+export const metadata = {
+    title :"Toutes les categories"
+}
 export default function Users(){
     const [layout, setLayout] = useState("grid")
     const [filter, setFilter] = useState("default")
@@ -24,6 +28,9 @@ export default function Users(){
             <span onClick={handleSetLayout} className="rounded cursor-pointer bg-gray-700 text-white flex items-center p-1 border">
                 {layout === "grid" ? <BsGrid3X3 className="h-4 w-4"/> : <BsListOl className="h-4 w-4"/>}
             </span>
+            <BaseBlueButton>
+                <BsPlus/>
+            </BaseBlueButton>
           </div>
         </div>
         <div className="flex mt-3 flex-wrap">
--- type.d.ts
+++ type.d.ts
@@ -1,6 +1,8 @@
+import React from "react"
+
 export type ButtonProps = {
-    text : string
-    icon?: JSX.Element
+    text ?: string
+    children ?: React.ReactNode
     iconPosition?:"left"|"right",
     width?:"full"|"fit"
     type?:"button"|"reset"|"submit"
