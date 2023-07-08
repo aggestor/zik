5668dbb5637be32ec92ffc2ae36c7be6516bd906
Aggestor <aggeer.aa@gmail.com>
2023-07-08 23:14:33 +0300

    add: error in textbox

--- components/TextBox.tsx
+++ components/TextBox.tsx
@@ -5,11 +5,16 @@ declare type Props = {
     name:string
     type?:string
     onChange?:(e:any)=> void
+    value?:string | number | readonly string[]
+    error?: Record<string, string[]>
 }
-const TextBox: FunctionComponent<Props> = ({label, placeholder, type = "text", name,onChange}) =>{
-    return <div className=" focus-within:text-blue-600 transition-all w-full duration-[400] focus-within:font-semibold my-2 text-gray-600 flex flex-col">
-            <label htmlFor={name} className="ml-1">{label}</label>
-            <input onChange={onChange} className="h-9 transition-all duration-300 focus:font-normal focus-within:ring-2 focus:ring-blue-600 px-1.5 border rounded-lg outline-none text-gray-700" name={name} id={name} placeholder={placeholder} type={type}/>
+const TextBox: FunctionComponent<Props> = ({label, placeholder, type = "text", name,onChange,value,error}) =>{
+    return<div>
+        <div className={`focus-within:text-blue-600 transition-all w-full duration-[400] focus-within:font-semibold ${error && error[name] ? ' text-red-600' : 'text-gray-600'}  flex flex-col`}>
+                <label htmlFor={name} className="ml-1">{label}</label>
+                <input value={value} onChange={onChange} className={`h-9 transition-all duration-300 focus:font-normal focus-within:ring-2 focus:ring-blue-600 px-1.5 border ${error && error[name] && 'border-red-600'} rounded-lg outline-none text-gray-700`} name={name} id={name} placeholder={placeholder} type={type}/>
+        </div>
+        {error && error[name] && <small className="text-red-600">{error[name][0]}</small>}
     </div>
 }
 
